7ee86e9b42a40611dc7090af7fead6f29933b3e3
Jethro <100454359+Whiplink@users.noreply.github.com>
2022-07-30 21:02:11 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const bars = document.querySelectorAll('.graph-container > div > div')
+
+fetch('./data.json')
+.then(res => res.json())
+.then(data => {
+  for(let i = 0; i < data.length; i++) {
+    const compute = x => `${x/60*100}%`
+    bars[i].style.height = compute(data[i].amount)
+    bars[i].addEventListener('mouseover', () => {
+      const div = document.createElement('div')
+      div.textContent = `$${data[i].amount}`
+      div.classList.add('hover')
+      bars[i].appendChild(div)
+      bars[i].div = div
+    })
+    bars[i].addEventListener('mouseout', () => {
+      bars[i].removeChild(bars[i].div)
+    })
+  }
+})
+const date = new Date()
+bars[date.getDay()].style.backgroundColor = "hsl(186, 34%, 60%)"
